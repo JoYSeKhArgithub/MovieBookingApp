2ce41f2182295ca92c2151b2a2f9f82452875278
Joysekhar Banerjee <bjoysekhar@gmail.com>
2026-01-17 18:14:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import connectDB from './db/index.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+
+app.get('/home',(req,res)=>{
+    return res.json({
+        success: true,
+        message: "fetching home"
+    })
+})
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Failed to connect to DB", err);
+})
